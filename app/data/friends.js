e9f3585061a0f4992823eea853522990879975a0
Emily Brown <emily_brown@rogers.com>
2019-08-24 09:52:19 -0400

    updated friend pics

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,50 +2,50 @@ var friends = [
 	
 	{
 		name: "Lizzie McGuire",
-		photo: "https://images.app.goo.gl/FW2DVkci6GFznMi9A",
+		photo: "https://imgix.bustle.com/uploads/image/2019/8/23/5b1bf728-fb85-4822-ad3e-e4d08d1919f9-5ddd8b70-7003-4557-86a3-5aafc5f34045-lizziemcguire.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70&dpr=2",
 		scores: [
+			"2",
 			"5",
-			"1",
-			"4",
 			"3",
-			"1",
-			"5",
-			"1",
-			"4",
 			"3",
-			"1"
-		]
-	},
-	{
-		name: "Raven Baxter",
-		photo: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-		scores: [
-			"3",
-			"1",
+			"2",
 			"5",
-			"4",
-			"1",
+			"2",
 			"5",
 			"1",
-			"1",
-			"5",
 			"3"
 		]
 	},
 	{
+		name: "Raven Baxter",
+		photo: "https://vignette.wikia.nocookie.net/thats-so-raven-fanon/images/9/92/Raven_Season_1.jpg/revision/latest/scale-to-width-down/1000?cb=20150525014520",
+		scores: [
+			"5",
+			"2",
+			"4",
+			"4",
+			"3",
+			"2",
+			"1",
+			"4",
+			"5",
+			"2"
+		]
+	},
+	{
 		name: "Hannah Montana",
-		photo: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		photo: "https://pmctvline2.files.wordpress.com/2016/11/hannah-montana-miley-cyrus.jpg?w=620&h=420&crop=1",
 		score: [
 			"3",
-			"1",
-			"4",
-			"1",
-			"4",
+			"2",
+			"5",
 			"3",
+			"2",
+			"5",
+			"4",
 			"5",
 			"1",
-			"5",
-			"4"		
+			"1"		
 		]
 	}
 ];
